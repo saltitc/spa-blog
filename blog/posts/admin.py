06469638fe2eac958829a5adcf1b342162ea1607
Ayvaz Izmailov <saltinthecut@gmail.com>
2023-08-23 17:07:02 +0300

    The post model is created, registered in the admin panel, migrations are created. Installed ckeditor, created its configuration in the project settings, added the path for it to the urls.py of the project. MEDIA_URL and MEDIA_ROOT are defined in the settings and added to the urls.py of the project.

--- blog/posts/admin.py
+++ blog/posts/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import Post
 
-# Register your models here.
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('title', 'description', 'pub_date', 'author')
+    fields = ('title', 'slug', 'description', 'content', 'image', 'pub_date', 'author')
+    prepopulated_fields = {'slug': ('title',)}
+    readonly_fields = ('pub_date',)
+    search_fields = ('title',)
+    ordering = ('-title',)
--- blog/posts/models.py
+++ blog/posts/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.contrib.auth.models import User
+from ckeditor_uploader.fields import RichTextUploadingField
 
-# Create your models here.
+
+class Post(models.Model):
+    title = models.CharField(max_length=200)
+    slug = models.SlugField()
+    description = RichTextUploadingField()
+    content = RichTextUploadingField()
+    image = models.ImageField()
+    pub_date = models.DateField(auto_now_add=True)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
